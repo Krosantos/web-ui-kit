97c98ba46c97bda738256f6543e5a350b2ba40c3
Tymko Tecca <tymko.tecca@gmail.com>
2023-08-03 15:08:26 -0400

    Storyboook (eww)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  extends: ["react-app", "prettier"],
+  extends: ["react-app", "prettier", "plugin:storybook/recommended"],
   plugins: ["grapes"],
   settings: {
     react: {
-      version: "detect",
-    },
+      version: "detect"
+    }
   },
   parser: "@typescript-eslint/parser",
   rules: {
     "grapes/no-internal-import": "error",
-    "import/no-anonymous-default-export": "off", // Storybook uses these
-  },
+    "import/no-anonymous-default-export": "off" // Storybook uses these
+  }
 };
